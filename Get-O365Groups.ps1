99a39d9adcc73c919e64c80c58e91c0fc7c782d0
LanLorde <bklawitter@gmail.com>
2023-08-01 16:45:52 -0500

    Added Header - Boner

--- Get-O365Groups.ps1
+++ Get-O365Groups.ps1
@@ -1,13 +1,26 @@
 <#
-TO DO:
-Convert this to work with Office 365
-
-
+.SYNOPSIS
+  <Overview of script>
+.DESCRIPTION
+  <Brief description of script>
+.PARAMETER <Parameter_Name>
+    <Brief description of parameter input required. Repeat this attribute if required>
+.INPUTS
+  <Inputs if any, otherwise state None>
+  None
+.OUTPUTS
+  <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+  None
+.NOTES
+  Version:        1.0
+  Author:         Brian Klawitter
+  Creation Date:  <Date>
+  Purpose/Change: Initial script development
+  Company:        Impact Networking
+  
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
 #>
-
-
-
-# Tests required Connection and or Installation of MSOnline Module
 
 Try {
 	$MsOn = ((Get-MsolCompanyInformation -ErrorAction Stop | Select-Object InitialDomain).InitialDomain) -split "\." -replace "\." | Select-Object -First 1
