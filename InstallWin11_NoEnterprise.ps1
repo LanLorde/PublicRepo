0a1ffda26cb12fd540d61e1ea2b8a5e0ac73ebcc
LanLorde <klawitterbrian@gmail.com>
2025-09-19 15:51:47 -0500

    commit

--- InstallWin11_NoEnterprise.ps1
+++ InstallWin11_NoEnterprise.ps1
@@ -8,7 +8,7 @@ Start-Process -FilePath $file -ArgumentList "/auto upgrade /quietinstall /eula a
 #>
 
 
-
+<#
 $workingdir = "C:\temp"
 $url = "https://go.microsoft.com/fwlink/?linkid=2171764"
 $file = "$($workingdir)\Win11Upgrade.exe"
@@ -21,3 +21,34 @@ New-Item -ItemType Directory -Force -Path $workingdir
 Invoke-WebRequest -Uri $url -OutFile $file
 
 Start-Process -FilePath $file -ArgumentList "/Install /QuietInstall /SkipEULA" -verb RunAs
+#>
+
+# Directory where Windows upgrade assistant exe will be downloaded.
+
+$dir = 'C:\temp\24H2'
+
+#This line will create the directory if it doesnt exist.
+
+mkdir $dir
+
+#This line will be used to download the file from the internet.
+
+$webClient = New-Object System.Net.WebClient
+
+#URL where Windows 11 upgrade assistant is hosted.
+
+$url = 'https://go.microsoft.com/fwlink/?linkid=2171764'
+
+#Variable that points to the upgrade exe.
+
+$file = "$($dir)\Win11Upgrade.exe"
+
+#This will grab the upgrade file from microsoft and save it to the specified file path in line 10.
+
+$webClient.DownloadFile($url,$file)
+
+# This will run Windows 11 Assistant and install it quietly, skips user license agreement, upgrades automatically
+
+# And copies the logs to the file path provided in line 3.
+
+Start-Process -FilePath $file -ArgumentList '/quietinstall /auto upgrade /NoRestartUI /finalize /skipeula /copylogs $dir'
